11a936500da7ae1fd87ff588bafae6426adfb8b0
keita813 <k1t37.smile@gmail.com>
2020-08-29 10:03:54 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email
+	before_save :create_activation_digest
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,14 +12,14 @@ class User < ApplicationRecord
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 	# 渡された文字列のハッシュ値を返す
-	def self.digest(string)
+	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
 		BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
 
 	# ランダムなトークンを返す
-	def self.new_token
+	def User.new_token
 		SecureRandom.urlsafe_base64
 	end
 
@@ -28,13 +30,39 @@ class User < ApplicationRecord
 	end
 
 	# 渡されたトークンがダイジェストと一致したらtrueを返す
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	# ユーザーのログイン情報を破棄する
 	def forget
 		update_attribute(:remember_digest,nil)
 	end
+
+	# アカウントを有効にする
+	def activate
+		update_attribute(:activated,    true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+  	# 有効化用のメールを送信する
+  	def send_activation_email
+  		UserMailer.account_activation(self).deliver_now
+  	end
+
+  	private
+
+  	
+	# メールアドレスをすべて小文字にする
+	def downcase_email
+		self.email = email.downcase
+	end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+    	self.activation_token  = User.new_token
+    	self.activation_digest = User.digest(activation_token)
+    end
 end
